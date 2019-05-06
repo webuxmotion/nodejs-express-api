a744f32060e6f203c47ce0dd253fcb4083964348
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2019-05-06 23:48:32 +0300

    Add user model

---
+++ README.md
@@ -0,0 +1,12 @@
+# pimpops-api-nodejs
+
+*/api/users/register*
+
+```json
+{
+	"email": "admin@admin.com",
+	"password": "password123",
+	"name": "Andrii",
+	"lastname": "Pereverziev"
+}
+```
--- server/server.js
+++ server/server.js
@@ -1,8 +1,39 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE);
 
 const port = process.env.PORT || 3002;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+// Models
+const { User } = require('./models/user');
+
+// =========
+// USERS
+// =========
+
+app.post('/api/users/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({success: false, err})
+    res.status(200).json({
+      success: true,
+      userdata: doc
+    });
+  });
+})
 
 app.listen(port, () => {
   console.log(`Server runing at ${port}`);
