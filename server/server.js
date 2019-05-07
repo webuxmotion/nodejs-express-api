99a9da6ed00024c3bfd718cae4fb2e25c22db6af
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2019-05-07 08:07:16 +0300

    Add login route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ const { User } = require('./models/user');
 // =========
 
 app.post('/api/users/register', (req, res) => {
+
   const user = new User(req.body);
 
   user.save((err, doc) => {
@@ -34,6 +35,35 @@ app.post('/api/users/register', (req, res) => {
     });
   });
 });
+
+app.post('/api/users/login', (req, res) => {
+
+  // find email
+  User.findOne({'email': req.body.email}, (err, user) => {
+
+    if (!user) return res.json({
+      loginSuccess: false, 
+      message: 'Auth failes, email not found'
+    }); 
+
+    // check password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) return res.json({
+        loginSuccess: false, 
+        message: 'Wrong password'});
+      
+      // generate a token
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);        
+
+        res.cookie('w_auth', user.token).status(200).json({
+          loginSuccess: true
+        });
+      });
+    });
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server runing at ${port}`);
