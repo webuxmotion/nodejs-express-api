ed5ed4c721962e023e2691d7523260bcdac394a3
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2019-05-08 17:42:12 +0300

    Create register_login router
    - Create Register_login component
    - Create MyButton component

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Home from './components/Home';
 import Layout from './hoc/layout';
+import Home from './components/Home';
+import RegisterLogin from './components/Register_login';
 
 const Routes = () => {
   return(
     <Layout>
       <Switch>
+        <Route path="/register_login" exact component={RegisterLogin} /> 
         <Route path="/" exact component={Home} /> 
       </Switch>
     </Layout>
