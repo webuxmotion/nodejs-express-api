91bcdbd93e3f776d1bfc304c20cb3a7b86e1e9f1
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2019-05-09 21:58:57 +0300

    Finishing register

--- client/src/reducers/user_reducer.js
+++ client/src/reducers/user_reducer.js
@@ -1,9 +1,12 @@
 import {
-  LOGIN_USER
+  LOGIN_USER,
+  REGISTER_USER
 } from '../actions/types';
 
 export default function(state={}, action) {
   switch (action.type) {
+    case REGISTER_USER:
+      return {...state, registerSuccess: action.payload}
     case LOGIN_USER:
       return {...state, loginSuccess: action.payload}
     default:
